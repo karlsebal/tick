49122dbf99fb56fda741dd8a1fd31878a1ae8362
karlsebal <karlsebal@posteo.de>
2017-11-28 06:05:19 +0100

    state

--- src/protocol.py
+++ src/protocol.py
@@ -21,11 +21,15 @@ class Protocol:
     :raises InvalidDateException: raised when the Date given is nonsense.
     """
 
+    HOURS_WORTH_HOLIDAY = 4 # a constant, how much a holiday is worth in hours
+
     def __init__(self, holidays_left: int=0, working_hours_account: int=0, month: str=None):
 
         self.protocol = []
-        self.holidays_left = holidays_left
-        self.working_hours_account = working_hours_account
+        self.holidays_left_begin = holidays_left
+        self.holidays_left = self.holidays_left_begin
+        self.working_hours_account_begin = working_hours_account
+        self.working_hours_account = self.working_hours_account_begin
         
         if not month:
             t = time.localtime()
@@ -54,5 +58,21 @@ class Protocol:
 
         if self.month < 1 or self.month > 12:
             raise InvalidDateException('%d is not a valid month' % self.month)
+
+    def append(self, tag:str, day:int, duration:int, from_unixtime:int, to_unixtime:int, description:str) -> None:
+        """
+        append an entry to the protocol
+
+        :param tag: tag
+        :param duration: duration of work
+        :param from_unixtime: beginning of work in epoch
+        :param to_unixtime: ending of work in epoch
+        :param description: description
+        """
+        self.protocol.append([tag, day, duration, from_unixtime, to_unixtime, description])
+
+    def __str__(self):
+        return str(self.protocol)
+
 
 # vim: ai sts=4 ts=4 sw=4 expandtab
